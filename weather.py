fdbf3b309f1a3ac7e70f8ebf6354d55ed73101f2
nonsignificantp <gus990@gmail.com>
2018-06-01 19:03:32 -0300

    Adds complete weather script

---
+++ weather.py
@@ -0,0 +1,121 @@
+import re
+import requests
+from datetime import datetime
+from concurrent import futures
+from pymongo import MongoClient
+from bs4 import BeautifulSoup as Soup
+
+def run():
+    with futures.ThreadPoolExecutor() as mmeExecutioner:
+        jobs = mmeExecutioner.map(fetch, addr)
+
+    if not jobs:
+        updateLog('No data was made available by fetch')
+        raise SystemExit
+
+    for data in list(jobs):
+        parser(data)
+
+    try:
+        connection = MongoClient()['ecobici']['weather']
+        connection.insert_one(weatherObj)
+    except:
+        updateLog('Mongo connection failed')
+        raise SystemExit
+
+    updateLog('Success with a total of {}'.format(weatherObj['responses']))
+
+def getIterNumber():
+
+    """Opens the file where the counter is, reads it and lastly adds one to it"""
+
+    with open('./data/weatherData.dat', 'r') as file:
+        num = file.read().strip()
+    
+    with open('./data/weatherData.dat', 'w') as file:
+        file.write(str(int(num)+1))
+    
+    return int(num)
+
+def fetch(addr):
+
+    """Fetch the data from the url that it is pass inside a list together with an id string"""
+
+    try:
+        response = requests.get(addr['url'], timeout = 10)
+        weatherObj['responses'] += 1
+        response = Soup(response.text, 'html.parser')
+        return [addr['station'], response]
+    except:
+        pass
+
+# ------------------------------------------------------------------------------
+
+def parser(data):
+    
+    """Updates the weather object. It's this ugly since weather sites are very different with each other"""
+    
+    if data[0] == 'ciuUba':
+        soup = data[1]
+        weatherObj['stations'].append(data[0])
+        weatherObj['temperature'].append(weather(soup.find_all('b')[5].text))
+        weatherObj['pressure'].append(weather(soup.find_all('b')[9].text))
+        weatherObj['dailyRain'].append(weather(soup.find_all('b')[13].text))
+        weatherObj['rainIntensity'].append(weather(soup.find_all('b')[15].text))
+        weatherObj['windVelocity'].append(weather(soup.find_all('b')[17].text))
+        weatherObj['windDirection'].append(wind(soup.find_all('b')[19].text))
+        weatherObj['RiseAndSet'] = riseandset(soup.find_all('b')[23].text)
+
+    if data[0] == 'laBoca':
+        soup = data[1]
+        weatherObj['stations'].append(data[0])
+        weatherObj['temperature'].append(weather(soup.find_all('p')[4].text))
+        weatherObj['pressure'].append(weather(soup.find_all('p')[29].text))
+        weatherObj['dailyRain'].append(weather(soup.find_all('p')[41].text))
+        weatherObj['rainIntensity'].append(weather(soup.find_all('font')[85].text))
+        weatherObj['windVelocity'].append(weather(soup.find_all('p')[33].text))
+        weatherObj['windDirection'].append(wind(soup.find_all('p')[34].text))
+
+def weather(string):
+    return float(re.findall(r'\d+\{}\d+'.format('.'), string)[0])
+
+def wind(string):
+    return re.findall(r'[^\W\d]+', string.strip())[0]
+
+def riseandset(string):
+    return re.findall(r'\d+\{}\d+'.format(':'), string)
+
+def updateLog(msg):
+
+    """Log inside ./log/ informes us about the exit value of the script"""
+
+    with open('./log/weatherLog.txt', 'a+') as file:
+        file.write('[{}]({}): {}\n'.format(weatherObj['timestamp'], weatherObj['id'], msg))
+    pass
+
+if __name__ == '__main__':
+    iterNumber = getIterNumber()
+    
+    weatherObj = {
+        'id': iterNumber,
+        'stations': [],
+        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'responses':0,
+        'temperature': [],
+        'pressure': [],
+        'dailyRain': [],
+        'rainIntensity': [],
+        'windVelocity': [],
+        'windDirection': [],
+        'RiseAndSet': ''
+    }
+    
+    addr = [{
+        'station':'laBoca',
+        'url':'http://www.bdh.acumar.gov.ar/bdh3/meteo/boca/mb1.htm'
+        },{
+        'station':'ciuUba',
+        'url':'http://estacion.at.fcen.uba.ar/estacion_tp.htm'
+        }]
+
+    run()
